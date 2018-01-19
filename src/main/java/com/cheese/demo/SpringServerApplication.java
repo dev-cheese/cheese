23d05fe7e844edb5b94c9ecd20bcb4f7d1015ba2
Yun <cheese10yun@gmail.com>
2018-01-20 03:13:18 +0900

    First Commit
    * Spring boot 프로젝트 생성
    * Application.yml JAP 및 Database 설정

---
+++ src/main/java/com/cheese/demo/SpringServerApplication.java
@@ -0,0 +1,12 @@
+package com.cheese.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringServerApplication.class, args);
+    }
+}
