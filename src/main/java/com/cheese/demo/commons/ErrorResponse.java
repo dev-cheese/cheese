63c97c31ea1948cc5d2daca8d8eef923377ee0a5
Yun <cheese10yun@gmail.com>
2018-02-06 02:46:13 +0900

    cheese-18 User reset API 개발 : Miner
    * 코트 정렬 및 사용하지 않는 코드 제거
    
    Signed-off-by: Yun <cheese10yun@gmail.com>

--- src/main/java/com/cheese/demo/commons/ErrorResponse.java
+++ src/main/java/com/cheese/demo/commons/ErrorResponse.java
@@ -2,13 +2,11 @@ package com.cheese.demo.commons;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.validation.FieldError;
 
 import java.util.List;
 
 @Getter
-@Setter
 @AllArgsConstructor
 public class ErrorResponse {
 
