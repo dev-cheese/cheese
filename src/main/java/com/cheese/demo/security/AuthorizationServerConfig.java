56234a91d483245935d176cb69be1e69c4bd65f7
Yun <cheese10yun@gmail.com>
2018-02-27 04:13:26 +0900

    cheese-38 JWT 도입
    Signed-off-by: Yun <cheese10yun@gmail.com>

---
+++ src/main/java/com/cheese/demo/security/AuthorizationServerConfig.java
@@ -0,0 +1,65 @@
+package com.cheese.demo.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    private final TokenStore tokenStore;
+    private final JwtAccessTokenConverter accessTokenConverter;
+    private final AuthenticationManager authenticationManager;
+    //    @Value("${security.jwt.client-id}")
+    private String clientId = "testjwtclientid";
+    //    @Value("${security.jwt.client-secret}")
+    private String clientSecret = "XY7kmzoNzl100";
+    //    @Value("${security.jwt.grant-type}")
+    private String grantType = "password";
+    //    @Value("${security.jwt.scope-read}")
+    private String scopeRead = "read";
+    //    @Value("${security.jwt.scope-write}")
+    private String scopeWrite = "write";
+    //    @Value("${security.jwt.resource-ids}")
+    private String resourceIds = "testjwtresourceid";
+
+    @Autowired
+    public AuthorizationServerConfig(TokenStore tokenStore, JwtAccessTokenConverter accessTokenConverter, AuthenticationManager authenticationManager) {
+        this.tokenStore = tokenStore;
+        this.accessTokenConverter = accessTokenConverter;
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer configurer) throws Exception {
+        configurer
+                .inMemory()
+                .withClient(clientId)
+                .secret(clientSecret)
+                .authorizedGrantTypes(grantType)
+                .scopes(scopeRead, scopeWrite)
+                .resourceIds(resourceIds);
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        TokenEnhancerChain enhancerChain = new TokenEnhancerChain();
+        enhancerChain.setTokenEnhancers(Arrays.asList(accessTokenConverter));
+        endpoints.tokenStore(tokenStore)
+                .accessTokenConverter(accessTokenConverter)
+                .tokenEnhancer(enhancerChain)
+                .authenticationManager(authenticationManager);
+    }
+
+
+}
---
+++ src/main/java/com/cheese/demo/security/ResourceServerConfig.java
@@ -0,0 +1,48 @@
+package com.cheese.demo.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    private final String ADMIN = "ADMIN";
+    private final String USER = "USER";
+    private final String MEMBER_URL_PATH = "/members";
+    private final ResourceServerTokenServices tokenServices;
+    //    @Value("${security.jwt.resource-ids}")
+    private String resourceIds = "testjwtresourceid";
+
+    @Autowired
+    public ResourceServerConfig(ResourceServerTokenServices tokenServices) {
+        this.tokenServices = tokenServices;
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        resources.resourceId(resourceIds).tokenServices(tokenServices);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .requestMatchers()
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, MEMBER_URL_PATH).permitAll()
+                .antMatchers(HttpMethod.GET, MEMBER_URL_PATH + "/exists**").permitAll()
+                .antMatchers(HttpMethod.GET, MEMBER_URL_PATH + "/{id}").hasRole(USER)
+                .antMatchers(HttpMethod.PUT, MEMBER_URL_PATH + "/{id}").hasRole(USER)
+                .antMatchers(HttpMethod.GET, MEMBER_URL_PATH + "/**").hasRole(ADMIN)
+                .anyRequest().denyAll();
+    }
+
+
+}
