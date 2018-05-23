2f4dceb7f3dada71d39762f8619cfd8abe9a5fa9
Yun <cheese10yun@gmail.com>
2018-05-24 02:11:53 +0900

    Fix
    Signed-off-by: Yun <cheese10yun@gmail.com>

--- src/test/java/com/cheese/demo/SpringServerApplicationTests.java
+++ src/test/java/com/cheese/demo/SpringServerApplicationTests.java
@@ -1,6 +1,5 @@
 package com.cheese.demo;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,7 +7,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@Ignore
 public class SpringServerApplicationTests {
 
     @Test
